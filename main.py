51b3c5791958e332494567a4807cbc5af1d081fc
Dandres <D4ndres@gmail.com>
2021-12-01 20:47:43 -0500

    Manteniedno lectura y plot del registro de lectura

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import serial
-#from datetime import datetime
+from matplotlib import pyplot as plt
 
 
 if __name__ == "__main__":
 
-	ser = serial.Serial("COM3", 115200)
-	ser.write(b'2')#La b significa byte. ascci caracter representacion de un unico caracter.
-
-	data = []
-	for i in range(100):
-		if i % 4 == 0 and i != 0:
-			#print( ''.join([chr(k) for k in data]) )
-			#print( ''.join(data).decode('utf-8') )
-			value = ''.join([k.decode('utf-8') for k in data]) #type string
-			print( value )
-			data = []
-		data.append(ser.read())
-
-		
-
-
-	ser.write(b'1')
-	ser.close()
-
+	with serial.Serial("COM3", 115200) as ser:	
+		ser.write(b'2')
+		data = []
+		y = []
+		i = 0
 	
+		try: 
+			while True:
+				if i % 4 == 0 and i != 0:
+					value = ''.join([k.decode('utf-8') for k in data]) #type string
+					print( value )
+					y.append( float(value) )
+					data = []
+				data.append(ser.read())
+				i += 1
+		except KeyboardInterrupt:
+			ser.write(b'1')
+			print("KeyboardInterrupt")
+			print(y)
+			plt.plot( y )
+			#plt.axis([-1, len(y) + 1, -(min(y)+0.01)/10, max(y) + max(y)/100])#Inecesario
+			plt.show()
