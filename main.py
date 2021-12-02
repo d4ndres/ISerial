350bc970d5b2441e057ba9f073225935adb2fc69
Dandres <D4ndres@gmail.com>
2021-12-01 21:04:14 -0500

    Reordenando código

--- main.py
+++ main.py
@@ -4,53 +4,51 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 
-ser = serial.Serial("COM3", 115200)
-
-
-fig, ax = plt.subplots()
-
-max_x = 100#ramaño de muestras
-max_rand = 5#valor imagen max
-min_rand = 0#valor imagen min
-
-x = np.arange(0, max_x)
-subx = np.arange(0, max_x)
-ax.set_ylim(min_rand, max_rand)
-
-line, = ax.plot(x, subx)#Los parametros son dos listas con el mismo tamaño. representa el eje x
-
-imagen = [np.nan] * len(x)
-
 def bytesToString(listOfBytes):
-	return float(''.join([k.decode('utf-8') for k in listOfBytes]))
+    return float(''.join([k.decode('utf-8') for k in listOfBytes]))
 
 
-def init():  # give a clean slate to start
+def init():
     line.set_ydata( imagen )
     return line,
 
 
-def animate(frame):  # update the y values (every 1000ms)
+def animate(frame): 
 
     data = []
     for i in range(4):
-    	data.append(ser.read())
+        some = ser.read()
+        data.append( some )
+    print( bytesToString(data) )
     imagen.append( bytesToString(data) )
-    imagen.pop(0)	
-  
+    imagen.pop(0)   
+    
     line.set_ydata(imagen)
 
     return line,
 
 def run():
-	ani = FuncAnimation(
-	    fig, animate, init_func=init, interval=5, blit=True, save_count=10)
 
-	plt.show()
+    ani = FuncAnimation(
+        fig, animate, init_func=init, interval=5, blit=True, save_count=10)
+    plt.show()
 
 if __name__ == "__main__":
-	#py -m serial.tools.listports
-	ser.write(b'2')
-	run()
-	ser.write(b'1')
-	ser.close()
+
+    ser = serial.Serial("COM3", 115200)
+    fig, ax = plt.subplots()
+    max_x = 100
+    max_rand = 1.2
+    min_rand = 0
+    x = np.arange(0, max_x)
+    subx = np.arange(0, max_x)
+    ax.set_ylim(min_rand, max_rand)
+    line, = ax.plot(x, subx)
+    imagen = [np.nan] * len(x)
+
+
+    #py -m serial.tools.listports
+    ser.write(b'2')
+    run()
+    ser.write(b'1')
+    ser.close()
