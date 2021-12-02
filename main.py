7807c076c23e1f8c2ba5aa78cf25ab13defe57cd
Dandres <D4ndres@gmail.com>
2021-12-01 20:55:49 -0500

    Lectura del serial y representacion con loop del MonitorPlotter

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 import serial
 from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
 
+
+ser = serial.Serial("COM3", 115200)
+
+
+fig, ax = plt.subplots()
+
+max_x = 100#ramaño de muestras
+max_rand = 5#valor imagen max
+min_rand = 0#valor imagen min
+
+x = np.arange(0, max_x)
+subx = np.arange(0, max_x)
+ax.set_ylim(min_rand, max_rand)
+
+line, = ax.plot(x, subx)#Los parametros son dos listas con el mismo tamaño. representa el eje x
+
+imagen = [np.nan] * len(x)
+
+def bytesToString(listOfBytes):
+	return float(''.join([k.decode('utf-8') for k in listOfBytes]))
+
+
+def init():  # give a clean slate to start
+    line.set_ydata( imagen )
+    return line,
+
+
+def animate(frame):  # update the y values (every 1000ms)
+
+    data = []
+    for i in range(4):
+    	data.append(ser.read())
+    imagen.append( bytesToString(data) )
+    imagen.pop(0)	
+  
+    line.set_ydata(imagen)
+
+    return line,
+
+def run():
+	ani = FuncAnimation(
+	    fig, animate, init_func=init, interval=5, blit=True, save_count=10)
+
+	plt.show()
 
 if __name__ == "__main__":
-
-	with serial.Serial("COM3", 115200) as ser:	
-		ser.write(b'2')
-		data = []
-		y = []
-		i = 0
-	
-		try: 
-			while True:
-				if i % 4 == 0 and i != 0:
-					value = ''.join([k.decode('utf-8') for k in data]) #type string
-					print( value )
-					y.append( float(value) )
-					data = []
-				data.append(ser.read())
-				i += 1
-		except KeyboardInterrupt:
-			ser.write(b'1')
-			print("KeyboardInterrupt")
-			print(y)
-			plt.plot( y )
-			#plt.axis([-1, len(y) + 1, -(min(y)+0.01)/10, max(y) + max(y)/100])#Inecesario
-			plt.show()
+	#py -m serial.tools.listports
+	ser.write(b'2')
+	run()
+	ser.write(b'1')
+	ser.close()
